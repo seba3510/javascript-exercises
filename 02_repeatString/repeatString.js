6bedeee7aba09e793008659b822038a4b94d2339
seba3510 <seba3510@protonmail.com>
2023-12-26 14:49:20 -0400

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,31 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+
+    // check if num is negative
+
+    if ((num < 0)) {
+        var err = "ERROR";
+        return err;
+
+    } //if()
+
+
+    if ((string == "")) {
+        return "";
+    }
+
+
+    const n = num;
+
+    let result = "";
+
+
+    for (let i = 0; i < n; i++) {
+
+        result += string;
+    } // for()
+
+
+    return result;
 
 };
 
