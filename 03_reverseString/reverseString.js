8e5708d2f96c004375f08b4f16cacdf3667b2c8e
seba3510 <seba3510@protonmail.com>
2023-12-26 14:55:25 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function (str) {
 
+    let result = "";
+
+    let n = str.length;
+
+    for (let index = n - 1; index >= 0; index--) {
+
+        const curr = str.charAt(index);
+
+        result += curr;
+    } // for()
+
+    return result;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
