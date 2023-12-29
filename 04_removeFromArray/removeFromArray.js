49a390bafa05ed813ee93be724533b02e6bfc5c3
seba3510 <seba3510@protonmail.com>
2023-12-29 12:36:54 -0400

    Complete exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,67 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+
+
+    if ((!contains(arr, args))) {
+        return arr;
+
+    } // if()
+
+
+
+    const n = arr.length;
+
+
+    // create copy of array
+    let copy = [];
+
+
+
+    for (let i = 0; i < n; i++) {
+
+
+        const elem = arr[i];
+
+        if ((!args.includes())) {
+
+            copy.push(elem);
+
+        }// if()
+
+
+
+    } // for()
+
+
+    return copy;
 
 };
+
+function contains(arr, key) {
+
+    const index = search(arr, key);
+
+    return ((index != -1));
+} // contains()
+
+function search(arr, key) {
+
+
+    const n = arr.length;
+
+
+
+    for (let index = 0; index < n; index++) {
+        const element = arr[index];
+
+        if ((element == key)) {
+            return index; // key found
+        } // if()
+
+
+    } //for()
+
+    return -1; // key not found
+} // search()
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
 
+
+    let min = a;
+    let max = b;
+
+
+    if ((typeof (min) != "number") || (typeof (max) != "number")) {
+        return "ERROR";
+    } // if()
+
+    if ((max < 0) || (min < 0)) {
+        return "ERROR";
+    }// if()
+
+    if ((min > max)) {
+        let temp = a;
+        min = max;
+        max = temp;
+    } // if()
+
+    let sum = 0;
+
+
+
+    let curr = min;
+    while ((curr <= max)) {
+
+        sum += curr;
+        curr++;
+    } // while()
+
+    return sum;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
