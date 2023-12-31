82e2d275158ae2ad637e50e3953f0093e43cd32b
seba3510 <seba3510@protonmail.com>
2023-12-31 13:46:25 -0400

    Complete exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,62 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+
+    str = removePunctuations(str.trim());
+
+    let rev = reverseString(str.trim());
+
+
+
+
+
+
+    return ((rev.toLowerCase() === str.toLowerCase()));
+
 
 };
 
+
+
+
+
+
+function reverseString(str) {
+
+
+    let result = "";
+
+
+
+    let n = str.length - 1;
+
+    for (let i = n; i >= 0; i--) {
+
+
+        result += str.charAt(i);
+
+
+    } // for()
+
+
+
+
+    return result.trim();
+} // reverseString()
+
+
+
+function removePunctuations(str) {
+
+    return str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+
+} // removePunctuations()
+
+
+
+
+
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,43 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+
+
+    if ((num < 0)) {
+
+        return "OOPS";
+
+
+    }
+
+    else if ((num == 0)) {
+
+        return 0;
+    }
+
+
+    let fib = new Array(num + 2);
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    let n = fib.length;
+
+
+    for (i = 2; i <= num; i++) {
+
+
+        let elem = fib[i - 1] + fib[i - 2];
+
+        fib[i] = elem;
+
+
+    } // for()
+
+
+
+    return fib[num]
+
+
 
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
