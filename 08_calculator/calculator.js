3c13eba60fb315d3781440f22e56456e7bbb58de
seba3510 <seba3510@protonmail.com>
2023-12-31 11:40:02 -0400

    Complete problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,112 @@
-const add = function() {
-	
+const add = function (a, b) {
+
+  return a + b;
 };
 
-const subtract = function() {
-	
+//============================================================
+
+
+const subtract = function (a, b) {
+
+  let diff = a - b;
+
+  return diff;
+
 };
 
-const sum = function() {
-	
+//============================================================
+
+
+const sum = function (array) {
+
+
+  let sum = 0;
+
+  let n = array.length;
+
+
+  for (let index = 0; index < n; index++) {
+    const element = array[index];
+
+    sum += element;
+
+  } // for()
+
+  return sum;
 };
 
-const multiply = function() {
+//============================================================
+
+
+const multiply = function (arr) {
+
+
+  let n = arr.length;
+
+  let result = 1;
+
+  for (let index = 0; index < n; index++) {
+    const element = arr[index];
+
+    result *= element;
+
+  } // for()
+
+
+  return result;
 
 };
 
-const power = function() {
-	
+
+//============================================================
+
+
+const power = function (base, exponent) {
+
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+//============================================================
+
+
+const factorial = function (num) {
+
+  if ((num == 0)) {
+
+    return 1;
+
+  }
+
+  else {
+
+    return num * factorial((num - 1));
+  }
 };
+
+//============================================================
+
+
+
+
+
+
+
+
+
+//============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
